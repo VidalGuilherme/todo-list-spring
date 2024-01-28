68b98bd34b6abc4c12a6b859bec11e25b6a0a133
Guilherme Fernandes Vidal <guileads@gmail.com>
2024-01-28 17:55:15 -0300

    created todo lists controller

---
+++ src/main/java/dev/vidal/todos/controllers/TodoListController.java
@@ -0,0 +1,68 @@
+package dev.vidal.todos.controllers;
+
+import dev.vidal.todos.dtos.TodoListDto;
+import dev.vidal.todos.models.TodoListModel;
+import dev.vidal.todos.repositories.TodoListRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/todo_lists")
+public class TodoListController {
+
+    @Autowired
+    private TodoListRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<TodoListModel>> getTodoLists(){
+        List<TodoListModel> todoListModels = repository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(todoListModels);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneTodoList(@PathVariable(value = "id") UUID id){
+        Optional<TodoListModel> todoList = repository.findById(id);
+
+        if(todoList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Todo List Not Found");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(todoList.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<TodoListModel> createTodoList(@RequestBody @Valid TodoListDto todoListDto){
+        TodoListModel todoListModel = new TodoListModel();
+        BeanUtils.copyProperties(todoListDto, todoListModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(todoListModel));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteTodoList(@PathVariable(value = "id") UUID id){
+        Optional<TodoListModel> todoList = repository.findById(id);
+        if(todoList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Todo List Not Found");
+        }
+        repository.delete(todoList.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Todo List deleted with success!");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateTodoList(@PathVariable(value = "id") UUID id, @RequestBody @Valid TodoListDto todoListDto){
+        Optional<TodoListModel> todoList = repository.findById(id);
+        if(todoList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Todo List Not Found");
+        }
+        TodoListModel todoListModel = todoList.get();
+        BeanUtils.copyProperties(todoListDto, todoListModel);
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(todoListModel));
+    }
+}
